0ca7ad9982ebb7782103bd6661e3b1cf23216f8d
ernads93 <ernadsehic@msn.com>
2016-12-02 21:30:26 +0100

    Task C

--- task1/src/segmentation.m
+++ task1/src/segmentation.m
@@ -5,20 +5,32 @@ function foreground_map = segmentation(frames,fg_scribbles,Hfc,Hbc,numbins)
 % Hbc .. color histogram of background pixels with colors grouped into bins
 % numbins .. the number of bins per channel to aggregate colors in histograms
 
+    [size_y, size_x, size_col, size_frame] = size(frames(:,:,:,:));
+    foreground_map = zeros(size_y, size_x, size_col);
+
     %----------------------------------------------------------------------
     % Task c: Generate cost-volume
     %----------------------------------------------------------------------
    
-    % color histograms give absolute counts in reference/scribbled frame
-    % normalize by total counts to get probability of bg pixel color
-    % as "cost" measure for fg pixel color
-    % we only have this cost measure for the refernce/scribbled frame!
-    % but we compare all other frames to it.
-    cost_fg = Hbc / (Hfc + Hbc); % cost for each bin
-    
-    % find cost of colors of scribbled pixel locations in each frame 
-    % for each pixel location in each frame: determine bin to look up cost
-    binIDs = getColHistBinIds(frames(1), frames(2), frames(3), numbins);
+    % To calculate the cost-volume matrice we need to visit each pixel of 
+    % the frame, and for each one calculate the cost-volume.
+    for frame_counter=1:size_frame
+        for count_y=1:size_y
+            for count_x=1:size_x
+                
+                % For each color-value of the pixels we need to determine
+                % the corresponded color-value in the "Bins"
+                imr = double(frames(count_y, count_x, 1, frame_counter));
+                img = double(frames(count_y, count_x, 2, frame_counter));
+                imb = double(frames(count_y, count_x, 3, frame_counter));
+                f = double(bins)/256.0;
+                relevant_bin = floor(imr*f) + floor(img*f)*bins + floor(imb*f)*bins*bins+1;
+                
+                % Now we use the founded Bin to get the volume-cost
+                foreground_map(count_y, count_x, frame_counter) = Hfc(relevant_bin)/(Hfc(relevant_bin)+Hbc(relevant_bin));
+            end
+        end
+    end
     
     %----------------------------------------------------------------------
     % Task e: Filter cost-volume with guided filter
